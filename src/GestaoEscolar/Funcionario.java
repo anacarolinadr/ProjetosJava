9ffb73e7e0b871efc45ea317129968dd36b0c9c4
AnaCarolina <passoscarol15@gmail.com>
2025-05-22 14:58:42 -0300

    Desafio Gestao Escolar - Java POO Iniciante
    Signed-off-by: AnaCarolina <passoscarol15@gmail.com>

---
+++ src/GestaoEscolar/Funcionario.java
@@ -0,0 +1,24 @@
+package GestaoEscolar;
+
+public class Funcionario extends Pessoa{
+    private String funcao;
+
+    public Funcionario(String nome, int idade, String funcao) {
+        super(nome, idade);
+        this.funcao = funcao;
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Função na escola: " + funcao);
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
