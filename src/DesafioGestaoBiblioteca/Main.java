8f10f5399c3408c8535ca9810464df0a42c477af
AnaCarolina <passoscarol15@gmail.com>
2025-05-26 15:59:05 -0300

    Desafio Gestao Biblioteca

---
+++ src/DesafioGestaoBiblioteca/Main.java
@@ -0,0 +1,22 @@
+package DesafioGestaoBiblioteca;
+
+public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Percy Jackson", "Rick", 2005);
+        Livro livro2 = new Livro("Harry Potter ", "JK", 1997);
+
+        livro1.emprestarLivro();
+
+        livro1.exibirInformacoes();
+        System.out.println("-----------");
+        livro2.exibirInformacoes();
+
+        livro1.devolverLivro();
+        livro2.emprestarLivro();
+
+        System.out.println("-----------");
+        livro1.exibirInformacoes();
+        System.out.println("-----------");
+        livro2.exibirInformacoes();
+    }
+}
