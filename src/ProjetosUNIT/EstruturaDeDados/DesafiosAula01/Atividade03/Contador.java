9603ef9a0d0433aa2abd9eaeda8a04cc8fc62aca
AnaCarolina <passoscarol15@gmail.com>
2025-08-13 19:28:43 -0300

    Atividade 03 ED - Correcao

--- src/ProjetosUNIT/EstruturaDeDados/DesafiosAula01/Atividade03/Contador.java
+++ src/ProjetosUNIT/EstruturaDeDados/DesafiosAula01/Atividade03/Contador.java
@@ -3,9 +3,12 @@ package ProjetosUNIT.EstruturaDeDados.DesafiosAula01.Atividade03;
 public class Contador {
 
     public void exibirValores(){
-        String sequencia = "1 2 3 4 5 6 7 8 9 10";
-        for (int i = 0; i < 11; i++) {
-            System.out.println(i + ", " + sequencia);
+        for (int i = 0; i <= 10; i++) {
+            System.out.print("( " + i + ", ");
+            for (int j = i; j <= 10; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(")");
         }
     }
 }
